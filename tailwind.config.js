c7fad8e30c867ecd4ece6b78fe037e7196a45d1a
samkaj <kajavasamuel@gmail.com>
2023-05-30 22:55:47 +0200

    Chore: tailwind stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 export default {
   content: ["./src/**/*.{html,svelte}"],
   theme: {
+    fontFamily: {
+      display: "Titillium Web, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+      sans: "Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+      mono: "Fira Code, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace",
+    },
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require("@tailwindcss/forms")],
+};
