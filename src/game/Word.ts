ca8d12c472e4a9bb940cb5ddbb831fda813372a4
samkaj <kajavasamuel@gmail.com>
2023-10-04 21:16:44 +0200

    Feat: word validation

--- src/game/Word.ts
+++ src/game/Word.ts
@@ -8,16 +8,62 @@ export enum State {
 class Word {
   value: Array<Letter>;
   state: State;
+  index: number;
 
   constructor(value: string) {
     this.value = value.split("").map((letter) => {
       return new Letter(letter);
     });
     this.state = State.INACTIVE;
+    this.index = 0;
   }
 
   public setState(state: State) {
     this.state = state;
+  }
+
+  public activate() {
+    this.setState(State.ACTIVE);
+  }
+
+  public handleInput(input: string) {
+    this.index++;
+    if (this.index >= this.value.length) {
+      this.setState(State.INCORRECT);
+      return;
+    }
+
+    const want = this.getCurrentLetter().value;
+    this.setCurrentLetterState(
+      want === input ? State.CORRECT : State.INCORRECT
+    );
+    
+    const isLast = this.index === this.value.length - 1;
+    if (isLast) {
+      this.validate();
+      return;
+    }
+
+    this.setNextLetterState(State.ACTIVE);
+  }
+
+  private validate() {
+    const isCorrect = this.value.every((letter) => letter.state === State.CORRECT);
+    this.setState(isCorrect ? State.CORRECT : State.INCORRECT);
+  }
+
+  private getCurrentLetter(): Letter {
+    return this.value[this.index];
+  }
+
+  private setCurrentLetterState(state: State) {
+    this.value[this.index].setState(state);
+  }
+
+  private setNextLetterState(state: State) {
+    if (this.index < this.value.length - 1) {
+      this.value[this.index+1].setState(state);
+    }
   }
 }
 
@@ -30,7 +76,7 @@ class Letter {
     this.state = State.INACTIVE;
   }
 
-  private setState(state: State) {
+  setState(state: State) {
     this.state = state;
   }
 }
